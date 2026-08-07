8774ab691cbc1c9602043c9bacc42d51b41f8fbb
only1mrjoshua <sorochijoshua30@gmail.com>
2026-08-07 23:13:59 +0100

    done

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,7 @@ const allowedOrigins = [
   'http://localhost:3001',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
+  'https://thecargogrid.com'
   // Add your production domains here later
 ];
 
